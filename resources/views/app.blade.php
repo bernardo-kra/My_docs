7f05773a6a5bf4d7a685f8c9f54aa0b5d26b6950
BernardoK <bernardo_kra@hotmail.com>
2023-05-28 16:02:02 -0300

    base

---
+++ resources/views/app.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>@yield('title')</title>
+    <style>
+        body{
+            background-color: #353740
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        @yield('content')
+    </div>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+    </script>
+
+</body>
+
+</html>
---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,12 @@
+@extends('app')
+
+@section('title', 'Dashboard')
+
+@section('content')
+    <div class="jumbotron">
+        <h1 class="display-4">Olá, bem-vindo ao Dashboard!</h1>
+        <p class="lead">Este é um template simples de painel ou dashboard para o Laravel utilizando Bootstrap.</p>
+        <hr class="my-4">
+        <p>Aqui você pode adicionar mais informações ou componentes para o seu dashboard.</p>
+    </div>
+@endsection
